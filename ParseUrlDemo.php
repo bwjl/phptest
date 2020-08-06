ae9acd86d505d2545f01c323d290cedadf45c393
bear <593788753@qq.com>
2020-08-07 00:15:01 +0800

    parse_url的用法

--- ParseUrlDemo.php
+++ ParseUrlDemo.php
@@ -21,3 +21,17 @@ var_dump(parse_url($url));
 //    ["fragment"]=>
 //  string(20) "/detail?goodsId=7983"
 //}
+
+$url = 'http://www.baidu.com/index.php?m=content&c=index&a=lists&catid=6&area=0&author=0&h=0®ion=0&s=1&page=1';
+var_dump(parse_url($url));
+//array(4) {
+//    ["scheme"]=>
+//  string(4) "http"
+//    ["host"]=>
+//  string(13) "www.baidu.com"
+//    ["path"]=>
+//  string(10) "/index.php"
+//    ["query"]=>
+//  string(71) "m=content&c=index&a=lists&catid=6&area=0&author=0&h=0®ion=0&s=1&page=1"
+//}
+
