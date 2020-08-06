570b3186cda88fd3d9a9ee367b3aaca895ea5d3e
bear <593788753@qq.com>
2020-08-06 15:48:54 +0800

    file_put_contents 用法 FILE_APPEND

---
+++ FilePutContentsDemo.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Created with PhpStorm.
+ * User: bear
+ * Date: 2020/8/6 下午3:42
+ * Description:
+ */
+
+file_put_contents('UpdateAdminUser.log', '表admin_user staff_id为 ' . 664 . '未匹配到表erp_user' . "\n", FILE_APPEND);
+file_put_contents('UpdateAdminUser.log', '表admin_user staff_id为 ' . 663 . '未匹配到表erp_user' . "\n", FILE_APPEND);
