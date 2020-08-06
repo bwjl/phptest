33c30d076465884ab72d8dc9d2fe7fcdbd77eccc
bear <593788753@qq.com>
2020-08-06 16:42:11 +0800

    intval 字符混合数字字符串

---
+++ IntvalDemo.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Created with PhpStorm.
+ * User: bear
+ * Date: 2020/8/6 下午4:40
+ * Description:
+ */
+
+var_dump(intval('goodsId=7983')); //int(0)
+var_dump((int)'goodsId=7983'); //int(0)
