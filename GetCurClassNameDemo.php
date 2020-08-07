88616bb83b34f9b4e2836ded123ab2ab834e6043
bear <593788753@qq.com>
2020-08-07 15:54:03 +0800

    __CLASS__ 获取当前类的名称，如何是父类那么是父类的名称

---
+++ GetCurClassNameDemo.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Created with PhpStorm.
+ * User: bear
+ * Date: 2020/8/7 上午9:38
+ * Description: __CLASS__ 获取当前类的名称，如何是父类那么是父类的名称
+ */
+
+class GetCurClassNameDemo
+{
+    public function getCurClassName()
+    {
+        return __CLASS__;
+    }
+}
+
+
+class GetCurClassNameDemoSon extends GetCurClassNameDemo
+{
+
+}
+
+var_dump((new GetCurClassNameDemo())->getCurClassName()); //getCurClassNameDemo
+var_dump((new GetCurClassNameDemoSon())->getCurClassName()); //getCurClassNameDemo
