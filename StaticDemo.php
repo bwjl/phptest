b9f3f7eeb7a07b20f6908299f3b94a47d44c695a
bear <593788753@qq.com>
2020-08-08 10:40:38 +0800

    关键字static用法 不允许被变量赋值

---
+++ StaticDemo.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created with PhpStorm.
+ * User: bear
+ * Date: 2020/8/8 上午10:36
+ * Description: 关键字static用法 不允许被变量赋值
+ */
+
+$a = [1, 2, 3, 4, 5, 6, 7, 8];
+
+$name = 'className' . time() . '.log';
+
+static $fileName = $name;
