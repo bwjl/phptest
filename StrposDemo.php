4337416bb390c34ef1b4d8e749f13f3b53b67529
bear <593788753@qq.com>
2020-08-11 14:27:26 +0800

    strpos的用法

--- StrposDemo.php
+++ StrposDemo.php
@@ -4,7 +4,11 @@
  * Created with PhpStorm.
  * User: bear
  * Date: 2020/8/6 下午4:44
- * Description:
+ * Description: strpos的用法
  */
 
 $url = 'https://shop.jc-saas.com/h5_project/#/detail?goodsId=7983';
+
+var_dump(strpos($url, 'http')); //0
+
+
