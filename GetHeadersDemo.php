6ccabd30c5c34de9f9608c07e7bb1ddf85af9381
bear <593788753@qq.com>
2020-08-11 17:38:34 +0800

    get_headers 的用法 检测url 状态码 200

---
+++ GetHeadersDemo.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created with PhpStorm.
+ * User: bear
+ * Date: 2020/8/11 下午5:32
+ * Description:
+ */
+
+$url = 'http://jccg.p.jc-test.cn/./file/template/user/133111/243_20200629113108_background_4p5nthmorja5i4605hr4ncl4u6.png';
+
+var_dump(get_headers($url));
+
+
+$url = './file/template/user/133111/243_20200629113108_background_4p5nthmorja5i4605hr4ncl4u6.png';
+
+var_dump(get_headers($url));
