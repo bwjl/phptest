af4dc4905e9d78ca173523152b09b745c49b006d
bear <593788753@qq.com>
2020-08-18 11:13:09 +0800

    php md5加密

---
+++ Md5Demo.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created with PhpStorm.
+ * User: bear
+ * Date: 2020/8/18 上午11:11
+ * Description: php md5加密
+ */
+
+//String password = "a12345679";
+//        String salt = "6y47";
+
+$password = 'a12345679';
+$salt = '6y47';
+
+var_dump(md5(md5($password . $salt)));
+
+
