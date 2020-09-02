94f6c30322633faabf50189256ec1d366da0f0a6
bear <593788753@qq.com>
2020-09-02 14:11:49 +0800

    json_encode JSON_UNESCAPE_UNICODE

--- JsonEncodeJsonUnescapedUnicodeDemo.php
+++ JsonEncodeJsonUnescapedUnicodeDemo.php
@@ -4,7 +4,7 @@
  * Created with PhpStorm.
  * User: bear
  * Date: 2020/9/2 下午1:53
- * Description:
+ * Description: json_encode JSON_UNESCAPE_UNICODE
  */
 
 $goodsJson = '[{"MessageID":"713964193DA642A579938D52B6C2A7CD","TAG":"data-synchronization","BODY":"{\"businessType\":\"SKU\",\"entityId\":7533,\"httpMethod\":\"get\",\"middleEndOperationType\":\"修改\",\"url\":\"\/merchandise\/finished-product\/sku\/7533\"}","nPublishTime":"1598582208194","FirstConsumeTime":"1598594124820","nConsumedTimes":"1","NextConsumeTime":"1598594424820","receiptHandles":["713964193DA642A579938D52B6C2A7CD-MTc3IDE1OTg1OTQxMjQ4MjAgMzAwMDAwIDcgMCBxZDlpbnRlcm5ldC0wMyA3IDIzMw=="]}]';
