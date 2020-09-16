f92eedb78e2ba7bff15db51c1945de5d7a84b6af
bear <593788753@qq.com>
2020-09-16 15:34:49 +0800

    count 字符串 等于 1

---
+++ CountStringDemo.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created with PhpStorm.
+ * User: bear
+ * Date: 2020/9/16 下午3:32
+ * Description:
+ */
+
+$str = '20200916';
+
+var_dump(count("20200916"));
+
+var_dump(count(explode('', '20200916')));
+
+var_dump(strlen($str));
---
+++ NullCompareZore.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Created with PhpStorm.
+ * User: bear
+ * Date: 2020/9/8 下午4:12
+ * Description:
+ */
+
+var_dump(null <= 0);
