8a2e30cac8e04f6772fe65c57524273da4ce3756
bear <593788753@qq.com>
2020-09-17 14:36:13 +0800

    substr 开始index 长度

---
+++ SubstrDemo.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created with PhpStorm.
+ * User: bear
+ * Date: 2020/9/17 下午2:34
+ * Description: substr 开始index 长度
+ */
+
+$str = '202009170000000062';
+
+var_dump(substr($str, 0, 8));
+var_dump(substr($str, -10));
---
+++ SwooleTaskDemo.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Created with PhpStorm.
+ * User: bear
+ * Date: 2020/9/17 下午1:42
+ * Description:
+ */
