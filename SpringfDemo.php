78bb8834d05ae78b6e6117447026a7e0fa957bd6
bear <593788753@qq.com>
2020-09-22 11:45:49 +0800

    springf的用法

---
+++ SpringfDemo.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created with PhpStorm.
+ * User: bear
+ * Date: 2020/9/22 上午11:40
+ * Description:
+ */
+
+
+$val1 = 5;
+$val2 = 0.007;
+
+var_dump(sprintf("%01.2f", $val1));
+var_dump(sprintf("%06.2f", $val1));
+var_dump(sprintf("%01.2f", $val2));
