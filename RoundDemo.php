f3b3e90d91a3675c4b2ae6f2ed68b03a83515c69
bear <593788753@qq.com>
2020-09-22 11:40:53 +0800

    round 四舍五入

---
+++ RoundDemo.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Created with PhpStorm.
+ * User: bear
+ * Date: 2020/9/21 上午10:46
+ * Description:
+ */
+
+$val1 = 79999.0000;
+
+var_dump(round($val1, 2));
+var_dump(sprintf("%01.2f", $val1));
+
+$val2 = 54.1256;
+var_dump(round($val2, 2));
+var_dump(sprintf("%01.2f", $val2));
+
+//var_dump();
+
+
+
