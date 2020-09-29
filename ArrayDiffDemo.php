8c1d1edaad9787c3dd27dd4f5d797fb4deb0ebfc
bear <593788753@qq.com>
2020-09-29 10:49:45 +0800

    微妙时间戳

---
+++ ArrayDiffDemo.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Created with PhpStorm.
+ * User: bear
+ * Date: 2020/9/24 下午4:15
+ * Description:
+ */
+
+
+$array1 = [
+    '009986',
+    '7899'
+];
+
+$array2 = [
+    '9986',
+    '9977'
+];
+
+var_dump(array_diff($array1, $array2));
---
+++ MicrotimeDemo.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created with PhpStorm.
+ * User: bear
+ * Date: 2020/9/29 上午10:46
+ * Description: 微妙时间戳
+ */
+
+var_dump(microtime());
+var_dump(microtime(true));
+var_dump(time());
