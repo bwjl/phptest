c4bf101b3d3366d2157f56e9aa74716291867b1c
bear <593788753@qq.com>
2020-10-28 14:37:39 +0800

    strpos 用法

---
+++ StrposDemo1.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created with PhpStorm.
+ * User: bear
+ * Date: 2020/10/27 下午6:38
+ * Description:
+ */
+
+
+$url = 'https://www.php.net/manual/zh/function.stripos.php';
+
+var_dump(strpos($url, 'http'));
