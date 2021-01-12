f52f977459ff65d7de60293fc36deac515098882
bear <593788753@qq.com>
2021-01-12 14:15:27 +0800

    preg_match 替换多个

--- PregMatchDemo.php
+++ PregMatchDemo.php
@@ -13,7 +13,32 @@ $url = 'https://shop.jc-saas.com/h5_project/#/detail?goodsId=7983&?goodsId=7984'
 preg_match('/goodsId=\d+/', $url, $matches);
 var_dump($matches);
 $ret = str_replace('goodsId=', '', $matches[0]);
-var_dump($ret);
+//var_dump($ret);
 
 preg_match_all('/goodsId=\d+/', $url, $matches1);
-var_dump($matches1);
+//var_dump($matches1);
+
+
+$str = '[品名]';
+
+//preg_match('/\[.+\]/', $str, $matches2);
+//var_dump($matches2);
+//
+//$str = str_replace(['[', ']'], '', $str);
+//
+//var_dump($str);
+
+$list = [
+    ['string' => '[品名]'],
+    ['string' => '[规格]'],
+
+];
+
+$array = [];
+foreach ($list as $item) {
+    if (preg_match('/\[.+\]/', $item['string'], $matches)) {
+        array_push($array, str_replace(['[', ']'], '', $matches[0]));
+    }
+}
+
+var_dump($array);
