b01593e1ef8c86a72cf34a7fcc882a6654ee36f3
bear <593788753@qq.com>
2021-01-13 10:48:07 +0800

    json_encode

---
+++ JsonEncodeDemo.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Created with PhpStorm.
+ * User: bear
+ * Date: 2020/11/5 上午11:40
+ * Description:
+ */
+
+
+//$array = [
+//    'username' => '13911050001',
+//    'password' => '982212cd510a8b143aea11b957822e55'
+//];
+//
+//var_dump(json_encode($array));
+
+$array = [
+    ['field_name' => '自定义1', 'field_value' => ''],
+    ['field_name' => '自定义2', 'field_value' => ''],
+    ['field_name' => '自定义3', 'field_value' => ''],
+    ['field_name' => '自定义4', 'field_value' => ''],
+    ['field_name' => '自定义5', 'field_value' => ''],
+    ['field_name' => '自定义6', 'field_value' => ''],
+    ['field_name' => '自定义7', 'field_value' => ''],
+    ['field_name' => '自定义8', 'field_value' => ''],
+    ['field_name' => '自定义9', 'field_value' => ''],
+    ['field_name' => '自定义10', 'field_value' => ''],
+    ['field_name' => '自定义11', 'field_value' => ''],
+    ['field_name' => '自定义12', 'field_value' => ''],
+    ['field_name' => '自定义13', 'field_value' => ''],
+    ['field_name' => '自定义14', 'field_value' => ''],
+    ['field_name' => '自定义15', 'field_value' => ''],
+    ['field_name' => '自定义16', 'field_value' => ''],
+    ['field_name' => '自定义17', 'field_value' => ''],
+    ['field_name' => '自定义18', 'field_value' => ''],
+    ['field_name' => '自定义19', 'field_value' => ''],
+    ['field_name' => '自定义20', 'field_value' => ''],
+];
+
+echo(json_encode($array, JSON_UNESCAPED_UNICODE));
