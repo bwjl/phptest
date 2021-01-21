4a9e7aa4995836ca7aaf28d81ca8671368d100d3
bear <593788753@qq.com>
2021-01-21 10:55:19 +0800

    打印class对象私有属性

---
+++ VarDumpClassPropertyDemo.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Created with PhpStorm.
+ * User: bear
+ * Date: 2021/1/21 上午10:47
+ * Description:  打印class对象私有属性
+ */
+
+
+class People
+{
+
+    private $name = "bear";
+    private $age = 18;
+
+
+}
+
+$people = new People();
+$people1 = new People();
+
+var_dump($people);
+var_dump($people1);
+
+
