9adc7e94ceaec82b5594c7f43b8b8f7f5004a54e
bear <593788753@qq.com>
2021-01-22 15:46:48 +0800

    大小写不一样 == 为false

--- SwitchEqualDemo.php
+++ SwitchEqualDemo.php
@@ -4,7 +4,7 @@
  * Created with PhpStorm.
  * User: bear
  * Date: 2021/1/22 下午3:42
- * Description:
+ * Description: 大小写不一样 == 为false
  */
 
 
@@ -22,4 +22,4 @@ switch ($method) {
 }
 
 var_dump($result);
-var_dump('post' == 'POST');
+var_dump('post' == 'POST'); //false
