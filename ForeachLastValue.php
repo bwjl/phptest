62a290edad874005ac16530472bce938dd680f15
bear <593788753@qq.com>
2021-02-06 19:56:55 +0800

    foreach 最后一个$value值会保留下来

--- ForeachLastValue.php
+++ ForeachLastValue.php
@@ -4,16 +4,16 @@
  * Created with PhpStorm.
  * User: bear
  * Date: 2021/1/18 下午2:53
- * Description: foreach 最后一个value值
+ * Description: foreach 最后一个$value值会保留下来
  */
 
 $array = [
-//    ['username' => '13986004080', 'password' => '123456'],
-//    ['username' => '13986004081', 'password' => '123457'],
+    ['username' => '13986004080', 'password' => '123456'],
+    ['username' => '13986004081', 'password' => '123457'],
 ];
 
 foreach ($array as $item) {
 
 }
 
-var_dump($item1);
+var_dump($item);
