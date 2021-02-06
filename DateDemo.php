5b1bbddc14d5f28847b960d914568f5e9ddbca40
bear <593788753@qq.com>
2021-02-06 19:54:02 +0800

    date 格式化时间戳

--- DateDemo.php
+++ DateDemo.php
@@ -7,9 +7,15 @@
  * Description:
  */
 
-class DateDemo
-{
 
-    //date('Y-m-d', strtotime('') + intval('') * 3600);
+$item = [
+    'product_year' => '2020-11-04',
+    'shelf_life' => 10,
+];
 
-}
+var_dump(strtotime($item['product_year']));
+
+$ret = date('Y-m-d', strtotime($item['product_year']) + intval($item['shelf_life']) * 3600 * 24);
+
+var_dump($ret);
+
