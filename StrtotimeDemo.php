ebcbaebfd7899bc561fb16af626932093f60f9e7
bear <593788753@qq.com>
2021-02-07 15:21:18 +0800

    strtotime 测试

--- StrtotimeDemo.php
+++ StrtotimeDemo.php
@@ -4,11 +4,17 @@
  * Created with PhpStorm.
  * User: bear
  * Date: 2020/8/4 下午2:38
- * Description:
+ * Description: strtotime 测试
  */
 
 
 $a = null;
 
-var_dump(strtotime($a)); //bool(false)
+//var_dump(strtotime($a)); //bool(false)
+
+$date = '2020.10.23';
+
+$time = strtotime(str_replace('.', '-', $date));
+
+var_dump(date('Y-m-d', $time));
 
