a2ae5fd7553486369e8d5722d31bd2e6860ee40d
bear <593788753@qq.com>
2021-04-29 14:08:08 +0800

    explode 支持null

---
+++ ExplodeNullDemo.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created with PhpStorm.
+ * @Author: bear
+ * @Date: 2021/4/29 2:07 下午
+ * @Description: explode 支持null
+ */
+
+var_dump(explode(',', null));
+
+//array(1) {
+//    [0]=>
+//  string(0) ""
+//}
