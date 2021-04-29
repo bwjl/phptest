63df8c54cbd4ba1104e1544ee192b4d822799bda
bear <593788753@qq.com>
2021-04-29 14:10:10 +0800

    explode 支持null 空字符串

--- ExplodeNullDemo.php
+++ ExplodeNullDemo.php
@@ -3,10 +3,11 @@
  * Created with PhpStorm.
  * @Author: bear
  * @Date: 2021/4/29 2:07 下午
- * @Description: explode 支持null
+ * @Description: explode 支持null 空字符串
  */
 
 var_dump(explode(',', null));
+var_dump(explode(',', ''));
 
 //array(1) {
 //    [0]=>
