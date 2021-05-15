8586966e4cfab5acc6b3b297206de380b5c10b8e
bear <593788753@qq.com>
2021-05-15 17:34:36 +0800

    json_decode

--- JsonDecodeDemo1.php
+++ JsonDecodeDemo1.php
@@ -10,7 +10,7 @@
 
 //$json = '[{"MessageID":"713964194D6242A5799317A3DFA94BC9","TAG":"data-synchronization","BODY":"{\"businessType\":\"Inventory\",\"entityId\":544,\"httpMethod\":\"get\",\"middleEndOperationType\":\"新增\",\"url\":\"\/inventory\/inventory\/sku-type-sum\/A1B88788802\"}","nPublishTime":"1599286215593","FirstConsumeTi":"1599286217689","nConsumedTimes":"1","NextConsumeTime":"1599286517689","receiptHandles":["713964194D6242A5799317A3DFA94BC9-NTc4IDE1OTkyODYyMTc2ODkgMzAwMDAwIDIgMCBxZDlpbnRlcm5ldC0wMSAwIDU3OA=="]}]';
 
-$json = '{"state":255,"msg":"appid\u4e0d\u80fd\u4e3a\u7a7a"}';
+$json = '{"goods_data":[{"order_sn":"210315162443310615","goods_sn":"A1B88478901","pay_time":1615796419},{"order_sn":"210311144228909232","goods_sn":"A2A78391901","pay_time":1615444996}]}';
 
 var_dump(json_decode($json, true));
 
--- JsonEncodeDemo.php
+++ JsonEncodeDemo.php
@@ -16,26 +16,12 @@
 //var_dump(json_encode($array));
 
 $array = [
-    ['field_name' => '自定义1', 'field_value' => ''],
-    ['field_name' => '自定义2', 'field_value' => ''],
-    ['field_name' => '自定义3', 'field_value' => ''],
-    ['field_name' => '自定义4', 'field_value' => ''],
-    ['field_name' => '自定义5', 'field_value' => ''],
-    ['field_name' => '自定义6', 'field_value' => ''],
-    ['field_name' => '自定义7', 'field_value' => ''],
-    ['field_name' => '自定义8', 'field_value' => ''],
-    ['field_name' => '自定义9', 'field_value' => ''],
-    ['field_name' => '自定义10', 'field_value' => ''],
-    ['field_name' => '自定义11', 'field_value' => ''],
-    ['field_name' => '自定义12', 'field_value' => ''],
-    ['field_name' => '自定义13', 'field_value' => ''],
-    ['field_name' => '自定义14', 'field_value' => ''],
-    ['field_name' => '自定义15', 'field_value' => ''],
-    ['field_name' => '自定义16', 'field_value' => ''],
-    ['field_name' => '自定义17', 'field_value' => ''],
-    ['field_name' => '自定义18', 'field_value' => ''],
-    ['field_name' => '自定义19', 'field_value' => ''],
-    ['field_name' => '自定义20', 'field_value' => ''],
+    'fillet_radius' => 2,
+    'list' => [
+        ['machine_id' => 28, 'label_spacing' => 2, 'print_length' => 1.15, 'status' => 1, 'material_type' => '1,2'],
+        ['machine_id' => 28, 'label_spacing' => 2, 'print_length' => 1.15, 'status' => 1, 'material_type' => '1,2'],
+    ],
+
 ];
 
 echo(json_encode($array, JSON_UNESCAPED_UNICODE));
