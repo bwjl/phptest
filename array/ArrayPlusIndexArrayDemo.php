90b2c7f4138a1832111e6a9ced9f12a3b4f6d23d
bear <593788753@qq.com>
2021-06-17 15:29:33 +0800

    index 数组相加 重复的index取第一个第一个值

---
+++ array/ArrayPlusIndexArrayDemo.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created with PhpStorm.
+ * @Author: bear
+ * @Date: 2021/6/17 3:24 下午
+ * @Description: index 数组相加 重复的index取第一个第一个值
+ */
+
+
+$arr1 = ['合成', '热敏'];
+$arr2 = ['PP'];
+var_dump($arr1 + $arr2);
+var_dump($arr2 + $arr1);
