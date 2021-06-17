59a66da51a208c7106f14d9de410b085841ed743
bear <593788753@qq.com>
2021-06-17 15:26:33 +0800

    array_merge 合成index 数组 直接累加

---
+++ array/ArrayMergerIndexArrayDemo.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created with PhpStorm.
+ * @Author: bear
+ * @Date: 2021/6/17 3:24 下午
+ * @Description: array_merge 合成index 数组 直接累加
+ */
+
+
+$arr1 = ['合成', '热敏'];
+$arr2 = ['PP'];
+var_dump(array_merge($arr1, $arr2));
