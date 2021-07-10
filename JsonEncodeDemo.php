2973d8e1941c0a2de38a1d650b436185fc0097f8
bear <593788753@qq.com>
2021-07-10 14:27:33 +0800

    json_encode

--- JsonEncodeDemo.php
+++ JsonEncodeDemo.php
@@ -16,12 +16,18 @@
 //var_dump(json_encode($array));
 
 $array = [
-    'fillet_radius' => 2,
-    'list' => [
-        ['machine_id' => 28, 'label_spacing' => 2, 'print_length' => 1.15, 'status' => 1, 'material_type' => '1,2'],
-        ['machine_id' => 28, 'label_spacing' => 2, 'print_length' => 1.15, 'status' => 1, 'material_type' => '1,2'],
+    'scheme_name' => '方案1',
+    'machine_id' => [9, 13],
+    'type' => 1,
+    'is_distinguish_width' => 1,
+    'ladder_list' => [
+        ['full_num' => 8, 'price_id' => 1, 'discount' => 7],
+        ['full_num' => 8, 'price_id' => 2, 'discount' => 7],
     ],
-
+    'width_list' => [
+        ['price_id' => 1, 'price_start' => 10, 'width_step' => 10, 'price_step' => 2],
+        ['price_id' => 2, 'price_start' => 10, 'width_step' => 10, 'price_step' => 2],
+    ],
 ];
 
 echo(json_encode($array, JSON_UNESCAPED_UNICODE));
