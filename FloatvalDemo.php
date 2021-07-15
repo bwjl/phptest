555da2e21f7700e217b56ec174ebe0886a731456
bear <593788753@qq.com>
2021-07-15 17:15:10 +0800

    floatval 转为float类型

---
+++ FloatvalDemo.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created with PhpStorm.
+ * @Author: bear
+ * @Date: 2021/7/15 5:13 下午
+ * @Description: floatval 转为float类型
+ */
+
+
+var_dump(floatval(0));
+var_dump(round(floatval(0), 2));
