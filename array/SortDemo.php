cffa5d33f79b929d43f5244296f0528e9b87d38c
bear <593788753@qq.com>
2021-07-17 15:00:01 +0800

    sort 对数组进行升序排序

---
+++ array/SortDemo.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created with PhpStorm.
+ * @Author: bear
+ * @Date: 2021/7/17 2:53 下午
+ * @Description: sort 对数组进行升序排序
+ *
+ */
+
+$arr = [5, 9, 1, 7, 8];
+
+//array_multisort()
+sort($arr);
+
+var_dump($arr);
